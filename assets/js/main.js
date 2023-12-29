c67359dce2170da32c8702dd4e9dba11a219f683
chaeaw <chaeaw04@gmail.com>
2023-12-29 15:29:36 +0900

    [#004 feat] mediaQuery 작업 완료
    - desktop, tablet, mobile 작업 완료
    - m-gnb 추가
    - .gitignore 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
-  let flag = 0;
-  $(window).resize(function () {
-    let viewWidth = $(window).offsetWidth();
-    console.log(viewWidth);
+  // m-gnb 활성화
+  $(".m-gnb-burger").click(function () {
+    $(this).siblings(".m-gnb-menu").addClass("active");
+  });
+
+  // m-gnb 아이템 클릭 시 비활성화
+  $(".m-gnb-item").click(function () {
+    $(".m-gnb-menu").removeClass("active");
   });
 });
