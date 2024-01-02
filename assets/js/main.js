30f78e353b062f3ff1f5f099387f2ac0a2453bc6
chaeaw <chaeaw04@gmail.com>
2024-01-02 17:57:27 +0900

    [#005 feat] fullpage 도입
    - fullpage scrollOverflow에서 막혔었음 -> body를 #fullpage로 잡은 것이 문제였음
    - onLeave를 사용해서 scroll시 해당하는 섹션의 .gnb-item.active 붙여줌
    - mediaQuery 일부 수정
    - mv text animation 도입
    - map img 일부 수정 및 contactFormBg 삽입

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
+  //gnb, m-gnb 아이템 클릭 시 item 활성화
+  $(".gnb-item").click(function () {
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
+  });
+
   // m-gnb 활성화
   $(".m-gnb-burger").click(function () {
     $(this).siblings(".m-gnb-menu").addClass("active");
   });
 
-  // m-gnb 아이템 클릭 시 비활성화
-  $(".m-gnb-item").click(function () {
+  // m-gnb 아이템, btn-close 클릭 시 menu 비활성화
+  $(".m-gnb-item, .btn-close").click(function () {
     $(".m-gnb-menu").removeClass("active");
   });
 });
